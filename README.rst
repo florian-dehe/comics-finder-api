fb2f598de107b31182dff2cc2e2398603e163059
Florian DEHE <florian.dehe@utt.fr>
2023-02-11 17:18:08 +0100

    Ready to prod

--- README.rst
+++ README.rst
@@ -2,3 +2,17 @@ Comics Finder API
 =================
 
 Comics Finder REST API build with the Django REST Framework
+
+Install
+-------
+``poetry install``
+
+Run Dev
+-------
+``poetry run ./comics_finder_api/manage.py runserver``
+
+Run Production
+-----
+``cd comics_finder_api``
+``poetry add gunicorn``
+``poetry run gunicorn --env DJANGO_SETTINGS_MODULE=comics_finder_api.settings_prod ./comics_finder_api/wsgi.py``
---
+++ comics_finder_api/comics_finder_api/settings/prod.py
@@ -0,0 +1,41 @@
+import environ
+import os
+
+
+from .dev import *
+
+env = environ.Env()
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# False if not in os.environ because of casting above
+DEBUG = env.bool('DEBUG')
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+# Parse database connection url strings
+# like psql://user:pass@127.0.0.1:8458/db
+DATABASES = {
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    'default': env.db(),
+}
+
+ALLOWED_HOSTS=env.list('ALLOWED_HOSTS')
+
+# HTTPS !
+CSRF_COOKIE_SECURE=env.bool('CSRF_COOKIE_SECURE')
+SESSION_COOKIE_SECURE=env.bool('SESSION_COOKIE_SECURE')
+SECURE_SSL_REDIRECT=env.bool('SECURE_SSL_REDIRECT')
+SECURE_HSTS_SECONDS=env.bool('SECURE_HSTS_SECONDS')
+SECURE_HSTS_INCLUDE_SUBDOMAINS=env.bool('SECURE_HSTS_INCLUDE_SUBDOMAINS')
+SECURE_HSTS_PRELOAD=env.bool('SECURE_HSTS_PRELOAD')
+
