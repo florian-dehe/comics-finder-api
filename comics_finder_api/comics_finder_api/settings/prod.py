9c8132c312f87224852d834bcb558969748b581f
Florian DEHE <florian.dehe@utt.fr>
2023-02-18 16:56:24 +0100

    Updated dependencies, added logging

--- comics_finder_api/comics_finder_api/settings/prod.py
+++ comics_finder_api/comics_finder_api/settings/prod.py
@@ -1,5 +1,6 @@
 import environ
 import os
+import logging.config
 
 
 from .dev import *
@@ -39,3 +40,33 @@ SECURE_HSTS_SECONDS=env.bool('SECURE_HSTS_SECONDS', False)
 SECURE_HSTS_INCLUDE_SUBDOMAINS=env.bool('SECURE_HSTS_INCLUDE_SUBDOMAINS', False)
 SECURE_HSTS_PRELOAD=env.bool('SECURE_HSTS_PRELOAD', False)
 
+
+# Logging Configuration
+
+# Clear prev config
+LOGGING_CONFIG = None
+
+# Get loglevel from env
+LOGLEVEL = os.getenv('DJANGO_LOGLEVEL', 'info').upper()
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+        '': {
+            'level': LOGLEVEL,
+            'handlers': ['console',],
+        },
+    },
+})
