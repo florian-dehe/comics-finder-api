ed04b294ee11393b92af4f012c7b051923a6fb09
Florian DEHE <florian.dehe@utt.fr>
2023-03-19 12:12:07 +0100

    Fix : Changed Auth system to Token

--- comics_finder_api/comics_finder_api/urls.py
+++ comics_finder_api/comics_finder_api/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('comics.urls')),
-    path('api/auth/', include('dj_rest_auth.urls'))
+    path("api/auth/login/", obtain_auth_token)
 ]
